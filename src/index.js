3c0cba50c57f753486d26da69989e0a335610c13
SOHAILASIF1 <alyana998877@gmail.com>
2023-12-25 03:00:06 +0500

    ji

--- src/index.js
+++ src/index.js
@@ -10,3 +10,12 @@ dotenv.config({
 
 
 connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+
+})
+.catch((err) =>{
+    console.log("Mongo db Connection Failed !!!" , err);
+})
