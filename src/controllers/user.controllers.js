dd4cae0a414535c886132055c7894063733a6e8f
SOHAILASIF1 <alyana998877@gmail.com>
2023-12-26 02:11:03 +0500

    jdee

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -35,7 +35,7 @@ const registerUser=asyncHandler(async (req,res) => {
         
 
     }
-    const existedUser=User.findOne(
+    const existedUser=await User.findOne(
         {
             $or:[{email},{username}]
         }
@@ -43,13 +43,13 @@ const registerUser=asyncHandler(async (req,res) => {
     if(existedUser){
         throw new ApiError(409, "User with email or user already existed")
     }
-    const avtarLocalPath=req.files?.avatar[0]?.path
+    const avatarLocalPath=req.files?.avatar[0]?.path;
     const coverImageLocalPath=req.files?.coverImage[0]?.path
-    if (!avtarLocalPath) {
+    if (!avatarLocalPath) {
         throw new ApiError(400,"Avatar File is required")
         
     }
-    const avatar=await uploadOnCloudinary(avtarLocalPath)
+    const avatar=await uploadOnCloudinary(avatarLocalPath)
     const coverImage=await uploadOnCloudinary(coverImageLocalPath)
      if (!avatar) {
         throw new ApiError(400,"Avatar File is required")
