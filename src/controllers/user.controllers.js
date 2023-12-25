53e6a21769e2e3fabdf08d118ecbc0bf8e0f5826
SOHAILASIF1 <alyana998877@gmail.com>
2023-12-25 20:43:25 +0500

    hi

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,42 @@
 import { asyncHandler } from '../utils/asyncHandler.js'
+import {ApiError} from '../utils/ApiError.js'
 const registerUser=asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message:"ok"
-    })
+    // get user detail from frontend
+    const {fullname,email,username,password } =req.body
+    
+    console.log("fullname",fullname);
+    console.log("fullname",password);
+    console.log("fullname",username);
+    console.log("fullname",email);
+
+    // validation
+    if(fullname==="" ){
+        throw new ApiError(400,"Full name is requird")
+        
+
+    } else if(email==="" ){
+        throw new ApiError(400,"email is requird")
+        
+
+    } else if(username==="" ){
+        throw new ApiError(400,"username is requird")
+        
+
+    }else if(password==="" ){
+        throw new ApiError(400,"password is requird")
+        
+
+    }
+    if(!email.includes('@')){
+        throw new ApiError(400,"valid email adress")
+        
+
+    }
+    
+    
+    
+    
+    
 })
+    
 export {registerUser}
